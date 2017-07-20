e378b0837aa9be4f884b37909fdb4714b9f3f0c7
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-07-20 00:07:02 -0300

    :smile: configure storybook with app default

---
+++ src/App.stories.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+import App from './App';
+
+storiesOf(`App`, module).add(`default app`, () => <App />);
