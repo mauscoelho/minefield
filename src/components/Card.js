f9744deac73aeb972e4ef0ef1bc016dfafb28617
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-07-21 01:00:52 -0300

    :\u2728 card with border shadows and fetching

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import propTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import diamont from '../images/diamond.png';
 import mine from '../images/mine.png';
 
 export const Colors = {
   backgroundRevealed: `#D2D6E6`,
   backgroundNotRevealed: `#FFFFFF`,
+  background: `#E2E4F0`,
 };
+
+const pulse = keyframes`
+0% { transform: scale(0.90); }
+25% { transform: scale(1.20); }
+50% { transform: scale(1); }
+75% { transform: scale(1.20); }
+100% { transform: scale(0.90); }
+`;
+
+const ContainerAnimated = styled.div`
+  display: flex;
+  flex: 1;
+  padding: 10px;
+  background-color: ${Colors.background};
+`;
 
 const Container = styled.div`
   display: flex;
   flex: 1;
   flex-direction: column;
-  padding: 10px;
+  padding: 6px;
   border-radius: 10px;
   justify-content: center;
   align-items: center;
+  box-shadow: 0 0 2px 1px
+    ${props =>
+      props.isRevealed
+        ? Colors.backgroundRevealed
+        : Colors.backgroundNotRevealed};
   background-color: ${props =>
     props.isRevealed
       ? Colors.backgroundRevealed
@@ -28,17 +49,28 @@ const Icon = styled.img`
   max-height: 90%;
 `;
 
-function Card({ isRevealed, isMine }) {
+const Pulse = styled(Container)`	
+   background-color: ${Colors.backgroundNotRevealed};   
+   animation: ${pulse} 2s infinite;
+   align-self: stretch;
+   box-shadow: 0 0 2px 1px ${Colors.backgroundNotRevealed};   
+  `;
+
+function Card({ isRevealed, isMine, isFetching }) {
   return (
-    <Container isRevealed={isRevealed}>
-      <Icon src={isMine ? mine : diamont} />
-    </Container>
+    <ContainerAnimated>
+      <Container isRevealed={isRevealed}>
+        {isFetching && <Pulse isRevealed={isRevealed} />}
+        {!isFetching && <Icon src={isMine ? mine : diamont} />}
+      </Container>
+    </ContainerAnimated>
   );
 }
 
 Card.propTypes = {
   isRevealed: propTypes.bool.isRequired,
   isMine: propTypes.bool.isRequired,
+  isFetching: propTypes.bool.isRequired,
 };
 
 export default Card;
--- src/components/Card.stories.js
+++ src/components/Card.stories.js
@@ -7,17 +7,22 @@ import Card from './Card';
 const Container = styled.div`
   display: flex;
   flex: 1;
-  width: 100px;
-  height: 100px;
+  width: 130px;
+  height: 130px;
 `;
 
-storiesOf(`Card`, module).add(`revealed with diamond`, () =>
+storiesOf(`Card`, module).add(`revealed with diamond and not fetching`, () =>
   <Container>
-    <Card isRevealed isMine={false} />
+    <Card isRevealed isMine={false} isFetching={false} />
   </Container>,
 );
-storiesOf(`Card`, module).add(`revealed with mine`, () =>
+storiesOf(`Card`, module).add(`revealed with mine and not fetching`, () =>
   <Container>
-    <Card isRevealed isMine />
+    <Card isRevealed isMine isFetching={false} />
+  </Container>,
+);
+storiesOf(`Card`, module).add(`not revelaled without mine and fetching`, () =>
+  <Container>
+    <Card isRevealed isMine={false} isFetching />
   </Container>,
 );
