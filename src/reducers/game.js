89f88f9511a60eff0a76c3eda7d9411f5fbdffbb
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2017-07-28 00:12:54 -0300

    :sparkles: reveal multiple

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -2,21 +2,36 @@ import Immutable from 'immutable';
 import { generateData } from '../cardGenerator';
 import { CARD_REVEAL } from '../constants';
 
-const cards = generateData({
+const initialCards = generateData({
   isMine: true,
 });
 
 const initialState = Immutable.fromJS({
-  cards,
+  cards: initialCards,
 });
+
+const revealOne = (state, action) =>
+  state.updateIn(
+    [`cards`, action.payload.id, `isRevealed`],
+    isRevealed => true,
+  );
+
+const revealMultiple = (state, action) =>
+  state.update(`cards`, cards =>
+    cards.map(
+      card =>
+        card.get(`id`) === action.payload.id
+          ? card.set(`isRevealed`, true)
+          : card.set(`halfRevealed`, true).set(`isRevealed`, true),
+    ),
+  );
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case CARD_REVEAL:
-      return state.updateIn(
-        [`cards`, action.payload.id, `isRevealed`],
-        isRevealed => true,
-      );
+      return action.payload.isMine
+        ? revealMultiple(state, action)
+        : revealOne(state, action);
     default:
       return state;
   }
